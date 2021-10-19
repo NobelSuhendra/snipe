892b9d968923fc6d40fa5a86a20c4c559fafd405
Nobel Suhendra <71556513+NobelSuhendra@users.noreply.github.com>
2021-10-19 23:01:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ async def on_ready():
     with open("store.json", "r") as read_file:
         data = json.load(read_file)
         delmsg = json.loads(data)
-    print(delmsg)
     await bot.change_presence(activity=discord.Activity(
         type=discord.ActivityType.watching, name="for deleted messages"))
 
@@ -27,11 +26,19 @@ async def on_ready():
 async def on_message_delete(ctx):
     if not ctx.author.bot:
         global delmsg
-        print(ctx.id)
-        delmsg[str(
-            ctx.id)] = [ctx.content,
-                        str(ctx.author),
-                        str(datetime.now())]
+        if str(ctx.guild.id) not in delmsg.keys():
+            delmsg[str(ctx.guild.id)] = {
+                str(ctx.id):
+                [ctx.content,
+                 str(ctx.author),
+                 str(datetime.now())]
+            }
+            print(0)
+        else:
+            delmsg[str(ctx.guild.id)][str(
+                ctx.id)] = [ctx.content,
+                            str(ctx.author),
+                            str(datetime.now())]
         os.remove("store.json")
         open("store.json", 'w').close()
         newdat = json.dumps(delmsg)
@@ -46,16 +53,23 @@ async def on_message_delete(ctx):
 @bot.command()
 async def snipe(ctx):
     global delmsg
-    embed = discord.Embed(title=f"**SNIPED!**\n\n─────────────")
-    for messages in reversed(list(delmsg.keys())):
+    if str(ctx.guild.id) not in delmsg.keys():
+        embed = discord.Embed(
+            title=f"No messages has been sniped in {ctx.guild}!")
+        embed.set_author(name=f"Requested by {ctx.author}",
+                         icon_url=ctx.author.avatar_url)
+        await ctx.channel.send(embed=embed)
+        return
+    embed = discord.Embed(title=f"**{ctx.guild} SNIPED List!**\n\n")
+    for messages in reversed(list(delmsg[str(ctx.guild.id)].keys())):
         embed.add_field(
             name=
-            f"\n**{delmsg[messages][1]}** deleted ({delmsg[messages][2]}):",
-            value=f'"{delmsg[messages][0]}"\n ───────────────',
+            f"\n**{delmsg[str(ctx.guild.id)][messages][1]}** deleted ({delmsg[str(ctx.guild.id)][messages][2]}):",
+            value=f'"{delmsg[str(ctx.guild.id)][messages][0]}"\n ',
             inline=False)
     embed.set_author(name=f"Requested by {ctx.author}",
                      icon_url=ctx.author.avatar_url)
     await ctx.channel.send(embed=embed)
 
 
-bot.run(os.getenv('token'))
+bot.run(os.getenv("token"))
